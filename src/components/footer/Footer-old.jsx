cd241b4290eeaa7fca6d3e619b28bf4824a7b983
ven-stu <115926101+ven-stu@users.noreply.github.com>
2022-12-02 22:17:33 +0200

    Add files via upload

---
+++ src/components/footer/Footer-old.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import { MDBFooter } from 'mdb-react-ui-kit';
+
+export function Footer() {
+  return (
+    <MDBFooter bgColor='light' className='text-center text-lg-left footer'>
+      <div className='text-center p-3 ' style={{ backgroundColor: 'rgba(0, 0, 0, 0.2)' }}>
+        &copy; {new Date().getFullYear()} Copyright:{' '}
+        <a className='text-dark' href='https://www.helpguide.org/articles/suicide-prevention/suicide-prevention.htm'>
+          VentsiIsStruggling.com
+        </a>
+      </div>
+    </MDBFooter>
+  );
+}
---
+++ src/components/footer/Footer.jsx
@@ -0,0 +1,11 @@
+import './Footer.scss';
+
+export function Footer() {
+    return (
+        <div className="footer">
+                <a className='text-dark' href='https://www.helpguide.org/articles/suicide-prevention/suicide-prevention.htm'>
+                VentsiIsStruggling.com
+                </a>
+            </div>
+    );
+}
